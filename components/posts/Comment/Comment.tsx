51c6caef12aabff12e9409774842b5e0b9a5fefe
SaraDonaldson <115354783+SaraDonaldson@users.noreply.github.com>
2025-03-26 13:02:55 +0000

    style comment text

--- components/posts/Comment/Comment.tsx
+++ components/posts/Comment/Comment.tsx
@@ -1,4 +1,5 @@
 import { PostComment } from '@/types/post'
+import { publishDisplayTime } from '@/utils/dateTime'
 import Link from 'next/link'
 import React from 'react'
 
@@ -9,10 +10,15 @@ function Comment({comment}:CommentProps) {
 
   return (
     <div className='flex flex-col gap-2 p-4 bg-slate-300 text-black'>
-        <div className='flex flex-row gap-2'>
-            <div className="rounded-xl border-cyan-600 border bg-slate-300 h-6 w-6"></div>
-            <span><Link href={comment.author.profileLink}>
-                   {comment.author.name}</Link></span>
+        <div className='flex flex-row gap-2 justify-between'>
+            <div className='flex flex-row gap-2'>
+                <div className="rounded-xl border-cyan-600 border bg-slate-300 h-6 w-6"></div>
+                <span className='text-cyan-800 font-semibold'><Link href={comment.author.profileLink}>
+                    {comment.author.name}</Link></span>
+            </div>
+            <div>
+                <span className='text-xs text-slate-500'>{publishDisplayTime(comment.dateCreated)}</span>
+            </div>
         </div>
         {comment.text &&
             <div className='text-slate-600'>
