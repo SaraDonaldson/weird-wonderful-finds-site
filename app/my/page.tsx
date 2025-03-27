a94899032966ca23dd6692f8367159cac29f2d80
SaraDonaldson <115354783+SaraDonaldson@users.noreply.github.com>
2025-03-27 14:11:25 +0000

    create dashboard and controller

---
+++ app/my/page.tsx
@@ -0,0 +1,11 @@
+import DashboardController from "@/components/controllers/DashboardController/DashboardController";
+
+
+
+export default function UserDashboard() {
+  return (
+    <main className="min-h-screen flex flex-col items-center">
+       <DashboardController/>
+    </main>
+  );
+}
---
+++ components/controllers/DashboardController/DashboardController.tsx
@@ -0,0 +1,10 @@
+'use client'
+import React from 'react'
+
+function DashboardController() {
+  return (
+    <div>DashboardController</div>
+  )
+}
+
+export default DashboardController
