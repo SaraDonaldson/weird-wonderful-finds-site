cb7bb6eee52647f3a77208e6b02af81c379ca9e3
SaraDonaldson <115354783+SaraDonaldson@users.noreply.github.com>
2025-04-02 07:58:42 +0100

    add toggle function

--- components/buttons/LikeButton/LikeButton.tsx
+++ components/buttons/LikeButton/LikeButton.tsx
@@ -12,13 +12,31 @@ function LikeButton({numberOfLikes, liked, toggleLikePostCallback}:LikeButtonPro
 
   const [isLiked, setIsLiked] = useState<boolean>(liked);
   const [likeCount, setLikeCount] = useState<number>(numberOfLikes);
-   const [animating, setAnimating] = useState<boolean>(false);
+  const [animating, setAnimating] = useState<boolean>(false);
   const [countAnimating, setCountAnimating] =useState<boolean>(false);
  
+
+   function handleToggle() {
+    //  Update like count
+    setLikeCount((c) => (isLiked ? c + 1 : c - 1));
+    // Update like state (local)   
+    setIsLiked(!isLiked);
+    //  Trigger icon transition animation
+    setAnimating(true);
+    setTimeout(() => setAnimating(false), 300);
+    // Trigger count animation with delay
+    setCountAnimating(true);
+    setTimeout(() => setCountAnimating(false), 300);
+    // toggle parent
+    toggleLikePostCallback();
+  }
   
   return (
-    <button className="flex items-center space-x-1 text-gray-600 dark:text-gray-400 
-                       hover:text-violet-brand dark:hover:text-cyan-accent transition">
+    <button 
+        className="flex items-center space-x-1 text-gray-600 dark:text-gray-400 
+                       hover:text-violet-brand dark:hover:text-cyan-accent transition"
+        onClick={()=>handleToggle()} 
+        >
       <Heart />
       <span className="text-sm">{likeCount}</span>
     </button>
