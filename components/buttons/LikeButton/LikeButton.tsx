eccc310b2ceca7a64f062b8e765b79d97b60af57
SaraDonaldson <115354783+SaraDonaldson@users.noreply.github.com>
2025-04-02 07:42:31 +0100

    create like button props interface

--- components/buttons/LikeButton/LikeButton.tsx
+++ components/buttons/LikeButton/LikeButton.tsx
@@ -2,7 +2,15 @@
 import { Heart } from 'lucide-react'
 import React from 'react'
 
-function LikeButton() {
+interface LikeButtonProps{
+    numberOfLikes: number
+    liked?:boolean
+    toggleLikePostCallback: ()=> void
+}
+
+function LikeButton({numberOfLikes, liked, toggleLikePostCallback}:LikeButtonProps) {
+
+
   return (
     <button className="flex items-center space-x-1 text-gray-600 dark:text-gray-400 
                        hover:text-violet-brand dark:hover:text-cyan-accent transition">
