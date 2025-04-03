afa073b68767d4b48c6cb85e69492a513fc00156
SaraDonaldson <115354783+SaraDonaldson@users.noreply.github.com>
2025-04-03 09:29:13 +0100

    add props and animation logic

--- components/buttons/CommentButton/CommentButton.tsx
+++ components/buttons/CommentButton/CommentButton.tsx
@@ -1,7 +1,24 @@
 import { MessageCircle } from 'lucide-react'
-import React from 'react'
+import React,{ useState } from 'react';
 
-function CommentButton() {
+interface CommentButtonProps{
+    triggerCommentCallback: ()=> void
+}
+
+function CommentButton({triggerCommentCallback}:CommentButtonProps) {
+    const [animating, setAnimating] = useState(false);
+
+
+  function handleClick() {
+    // Callback to trigger comment box
+    triggerCommentCallback()
+    // animate on click
+    setAnimating(true);
+    setTimeout(() => setAnimating(false), 200);
+  }
+
+   
+
   return (
      <button className="flex items-center space-x-1 text-gray-600 dark:text-gray-400 
                        hover:text-violet-brand dark:hover:text-cyan-accent transition">
