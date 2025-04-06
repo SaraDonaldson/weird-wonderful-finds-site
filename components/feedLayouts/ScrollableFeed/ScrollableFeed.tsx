97c2529c521099f19960ff6c23111e3bf6b83aca
SaraDonaldson <115354783+SaraDonaldson@users.noreply.github.com>
2025-04-06 11:12:31 +0100

    adjust top spacing

--- components/feedLayouts/ScrollableFeed/ScrollableFeed.tsx
+++ components/feedLayouts/ScrollableFeed/ScrollableFeed.tsx
@@ -9,7 +9,7 @@ interface ScrollableFeedProps{
 function ScrollableFeed({posts}:ScrollableFeedProps) {
 
   return (
-    <div className="w-3/5 h-screen mx-auto px-10 py-10 flex-col">
+    <div className="w-3/5 h-screen mx-auto px-10 py-10 flex-col mt-32">
         <div className="flex flex-col h-screen">
 
             {posts?.map((post:PostType, i) => {
