0ff5a008599a3c42bc903c2b558c5a9eca99cb1d
SaraDonaldson <115354783+SaraDonaldson@users.noreply.github.com>
2025-04-13 09:05:31 +0100

    create explore and search page

---
+++ app/explore/page.tsx
@@ -0,0 +1,9 @@
+import ExploreController from '@/components/controllers/ExploreController/ExploreController'
+
+function Explore() {
+  return (
+    <div><ExploreController/></div>
+  )
+}
+
+export default Explore
