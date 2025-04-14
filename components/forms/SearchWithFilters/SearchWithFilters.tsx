6fec758ab55b50fcb14111b2ec835f199a1b7e3c
SaraDonaldson <115354783+SaraDonaldson@users.noreply.github.com>
2025-04-14 11:21:52 +0100

    create search component

---
+++ components/forms/SearchWithFilters/SearchWithFilters.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+
+
+function SearchWithFilters() {
+  return (
+    <div>SearchWithFilters
+
+            {/* search bar => on click, opens drawer with filters */}
+            {/* filters drawer */}
+
+    </div>
+  )
+}
+
+export default SearchWithFilters
